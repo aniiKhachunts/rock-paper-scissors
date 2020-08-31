239825df1a6f5f15e5c324d5230997264c938bba
Ani Khachunts <khachunts.ani@gmail.com>
2020-08-31 13:27:44 +0400

    Added initial files and some styles.

---
+++ app.js
@@ -0,0 +1,19 @@
+const game = () => {
+  let pScore = 0;
+  let cScope = 0;
+
+  // start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  startGame();
+};
+game();
