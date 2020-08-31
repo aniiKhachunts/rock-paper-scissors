c38097be39b98425a6bf3550d4250207d75deeae
Ani Khachunts <khachunts.ani@gmail.com>
2020-08-31 14:47:27 +0400

    Added the big part of the functionality. Now the app knows when and who wins and who loses. It does even know when it's a tie!

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const game = () => {
   let pScore = 0;
-  let cScope = 0;
+  let cScore = 0;
 
-  // start the game
+  //Start the Game
   const startGame = () => {
     const playBtn = document.querySelector(".intro button");
     const introScreen = document.querySelector(".intro");
@@ -13,7 +13,85 @@ const game = () => {
       match.classList.add("fadeIn");
     });
   };
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    // computer options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        compareHands(this.textContent, computerChoice);
+        //update images
+        playerHand.src = `./images/${this.textContent}.png`;
+        computerHand.src = `./images/${computerChoice}.png`;
+      });
+    });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    const winner = document.querySelector(".winner");
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie";
+      return;
+    }
+    //check for the rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer wins!";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //check for the paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer Wins!";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player wins!";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    //check for scissors
+
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer Wins!";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player wins!";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
 
   startGame();
+  playMatch();
 };
 game();
