45ea5e88a4ff3ecb14a5460f30aff11bbf1471ab
Ani Khachunts <khachunts.ani@gmail.com>
2020-08-31 17:01:57 +0400

    Added final functionality and animation

--- app.js
+++ app.js
@@ -13,22 +13,37 @@ const game = () => {
       match.classList.add("fadeIn");
     });
   };
+  //Play Match
   const playMatch = () => {
     const options = document.querySelectorAll(".options button");
     const playerHand = document.querySelector(".player-hand");
     const computerHand = document.querySelector(".computer-hand");
-    // computer options
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach((hand) => {
+      hand.addEventListener("animationend", function () {
+        this.style.animation = "";
+      });
+    });
+    //Computer Options
     const computerOptions = ["rock", "paper", "scissors"];
 
     options.forEach((option) => {
       option.addEventListener("click", function () {
+        //Computer Choice
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
 
-        compareHands(this.textContent, computerChoice);
-        //update images
-        playerHand.src = `./images/${this.textContent}.png`;
-        computerHand.src = `./images/${computerChoice}.png`;
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `./images/${this.textContent}.png`;
+          computerHand.src = `./images/${computerChoice}.png`;
+        }, 2000);
+        //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
       });
     });
   };
@@ -41,49 +56,50 @@ const game = () => {
   };
 
   const compareHands = (playerChoice, computerChoice) => {
+    //Update Text
     const winner = document.querySelector(".winner");
+    //Checking for a tie
     if (playerChoice === computerChoice) {
       winner.textContent = "It is a tie";
       return;
     }
-    //check for the rock
+    //Check for Rock
     if (playerChoice === "rock") {
       if (computerChoice === "scissors") {
-        winner.textContent = "Player Wins!";
+        winner.textContent = "Player Wins";
         pScore++;
         updateScore();
         return;
       } else {
-        winner.textContent = "Computer wins!";
+        winner.textContent = "Computer Wins";
         cScore++;
         updateScore();
         return;
       }
     }
-    //check for the paper
+    //Check for Paper
     if (playerChoice === "paper") {
       if (computerChoice === "scissors") {
-        winner.textContent = "Computer Wins!";
+        winner.textContent = "Computer Wins";
         cScore++;
         updateScore();
         return;
       } else {
-        winner.textContent = "Player wins!";
+        winner.textContent = "Player Wins";
         pScore++;
         updateScore();
         return;
       }
     }
-    //check for scissors
-
+    //Check for Scissors
     if (playerChoice === "scissors") {
       if (computerChoice === "rock") {
-        winner.textContent = "Computer Wins!";
+        winner.textContent = "Computer Wins";
         cScore++;
         updateScore();
         return;
       } else {
-        winner.textContent = "Player wins!";
+        winner.textContent = "Player Wins";
         pScore++;
         updateScore();
         return;
@@ -91,7 +107,10 @@ const game = () => {
     }
   };
 
+  //Is call all the inner function
   startGame();
   playMatch();
 };
+
+//start the game function
 game();
